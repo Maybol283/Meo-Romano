20fa6fd0262e58fc8ebdf17893b1dbd0ba46918b
Maybol283 <126118241+Maybol283@users.noreply.github.com>
2024-01-16 20:20:38 +0000

    homepage needs animations for dialog transition

--- src/pages/header.jsx
+++ src/pages/header.jsx
@@ -3,10 +3,11 @@ import { Dialog } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
 const navigation = [
-  { name: "Menu", href: "#" },
+  { name: "Home", href: "#" },
   { name: "Reservation", href: "#" },
   { name: "Gallery", href: "#" },
   { name: "About Us", href: "#" },
+  { name: "Menu", href: "#" },
 ];
 
 export default function Header() {
@@ -50,13 +51,9 @@ export default function Header() {
         open={mobileMenuOpen}
         onClose={setMobileMenuOpen}
       >
-        <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <h2 className="h-8 w-auto text-black text-xl pl-2">Meo Romano</h2>
-            </a>
+        <div className="fixed inset-0 z-10 " />
+        <Dialog.Panel className="overflow-y-hidden fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 md:max-w-md sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-end pt-1">
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -66,14 +63,14 @@ export default function Header() {
               <XMarkIcon className="h-6 w-6" aria-hidden="true" />
             </button>
           </div>
-          <div className="mt-6 flow-root">
+          <div className="flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
+              <div className="pb-6 md:text-left h-svh flex flex-col justify-around ">
                 {navigation.map((item) => (
                   <a
                     key={item.name}
                     href={item.href}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    className="block rounded-lg  md:text-left text-center text-3xl font-semibold leading-10 text-gray-900 hover:bg-gray-50"
                   >
                     {item.name}
                   </a>
