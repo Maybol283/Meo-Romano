c37826651ff0f25fe28fcbbe5cbddec1be7b270f
Maybol283 <126118241+Maybol283@users.noreply.github.com>
2024-01-16 16:09:40 +0000

    homepage frontend done, needs href links

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
 import "./App.css";
+import Header from "./pages/header";
 import Homepage from "./pages/homepage";
 
 function App() {
   return (
     <>
+      <Header></Header>
       <Homepage></Homepage>
     </>
   );
