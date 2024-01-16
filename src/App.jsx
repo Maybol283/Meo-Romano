37f2b2605067492352566a10f24ee02bff2fe22b
Maybol283 <126118241+Maybol283@users.noreply.github.com>
2024-01-16 12:59:48 +0000

    Inital commit homepage design started

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import Homepage from "./pages/homepage";
+
+function App() {
+  return (
+    <>
+      <Homepage></Homepage>
+    </>
+  );
+}
+
+export default App;
