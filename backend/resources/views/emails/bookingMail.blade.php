7397520f221bc6a9918a0e40f0d598e63399e335
Maybol283 <126118241+Maybol283@users.noreply.github.com>
2024-02-11 10:13:44 +0000

    updated email styling

--- backend/resources/views/emails/bookingMail.blade.php
+++ backend/resources/views/emails/bookingMail.blade.php
@@ -4,23 +4,23 @@
     <title>Booking Confirmation</title>
     <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="{{ asset('frontend/assets/index-abf05e8a.css') }}" rel="stylesheet">
+  <link href="{{ asset('frontend/assets/index-6e2d9d38.css') }}" rel="stylesheet">
 </head>
-<body class="">
-    <div class="bg-gray-100 h-screen w-screen flex place-items-center">
-    <div class="max-w-md mx-auto mt-8 bg-white shadow-md rounded-md">
-        <div class="px-6 py-8">
-            <h1 class="text-2xl mb-4">Booking Confirmation</h1>
-            <p class="my-4">Hello <span class="font-bold">{{ $data['first_name'] }}</span>,</p>
-            <p class="mb-4">Thank you for your booking. Here are the details of your reservation:</p>
-            <ul class="mb-4">
-                <li>Time Slot: {{ $data['time_slot'] }}</li>
-                <li>Date: {{ $data['date'] }}</li>
+<body class="flex place-content-center">
+    <div class="bg-gray-100 h-50 w-screen">
+    <div class="mx-auto mt-8 bg-white shadow-md rounded-md">
+        <div class="px-6 py-20">
+            <h1 class="text-2xl mx-auto">Booking Confirmation</h1>
+            <p class="my-10">Hello <span class="font-bold">{{ $data['first_name'] }}</span>,</p>
+            <p class="pb-10">Thank you for your booking. Here are the details of your reservation:</p>
+            <ul class="flex flex-col gap-y-1 pb-10">
+                <li>Time Slot: <span class="font-semibold">{{ $data['time_slot'] }}</span></li>
+                <li>Date: <span class="font-semibold">{{ $data['date'] }}</span></li>
             </ul>
-            <p>We look forward to seeing you!</p>
+            <p class="pb-10">We look forward to seeing you!</p>
         </div>
         <div class="px-6 py-4 bg-gray-200 text-sm text-gray-600">
-            <p>This email was sent from My Company. Please do not reply to this email.</p>
+            <p>This is an automated message. Please do not reply to this email.</p>
         </div>
     </div>
     </div>
